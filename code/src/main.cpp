e01ad9fddcc9a0efa497508f44e23f13c7e09666
Georgii Zhulikov <l8doku@gmail.com>
2023-02-20 16:20:25 +0500

    Workshopp 12 progress

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -11,6 +11,7 @@ using std::endl;
 
 
 typedef std::vector<int> VecInt;
+using MapInt = std::map<int, size_t>;
 
 // Generating random numbers:
 
@@ -30,23 +31,94 @@ VecInt generateVector(size_t n, int rangeStart, int rangeEnd)
 }
 
 
+
+using MapInt = std::map<int, size_t>;
+
+
 // consider using range-based loops for printing
 void printVec(const VecInt& vecInt);
 
-//void printMap(const MapInt& mapInt)
 
 
+void printMap(const MapInt& mapInt)
+{
+    int t = 1;
+    for(MapInt::const_iterator it = mapInt.begin(); it != mapInt.end(); std::advance(it, t))
+    {
+        cout << "Key is " << it->first;
+        cout << " Value is " << it->second << endl;
+
+        for (int i = 0; i < t; ++i)
+        {
+            ++it;
+        }
+    }
+
+    for(const std::pair<const int, size_t>& kv_pair: mapInt)
+    {
+        cout << "Key is " << kv_pair.first;
+        cout << " Value is " << kv_pair.second << endl;
+    }
+}
+
+
+MapInt makeMap(const VecInt& v)
+{
+    MapInt mapInt;
+    for(int item: v)
+    {
+        if (mapInt.count(item) != 0) // item is in map
+            ++mapInt[item];
+        else
+            mapInt[item] = 1;
+    }
+    return mapInt;
+}
+
+
+bool query(const std::map<int, size_t>& m, int k, size_t& element)
+{
+    k = 1000;
+}
 
 
 int main()
 {
-
     cout << "Enter three integers: n, r, q\n";
     int n;
     int r;
     int q;
+    //        4    10   1000
+    // cin >> n >> r >> q;
+    //  18 200 494 900
 
-    cin >> n >> r >> q;
+
+
+    // VecInt v = generateVector(n, r, q);
+    // 1 2 3 2 1 2 1 2 2 3 1 
+
+    // MapInt m = makeMap(v)
+
+    //{
+    //    1: 4,
+    //    2: 5,
+    //    3: 2
+    //}
+    MapInt m;
+    m[18] = 1;
+    m[200] = 1;
+    printMap(m);
+    // cout << m[0] << endl;
+
+
+    bool success;
+    size_t value18 = 0;
+    int k = 18;
+    success = query(m, k, value18);
+
+    // success = true
+    // value18 = 1
+    // k = 18
 
 
     return 0;
